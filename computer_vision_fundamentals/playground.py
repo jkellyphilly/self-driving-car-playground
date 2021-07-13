0ec3f869a28934bf7ac69ffeb095c6f3230b85dc
jkellyphilly <joel.kelly.philly@gmail.com>
2021-07-13 08:39:23 -0400

    finding the lane with CV

---
+++ computer_vision_fundamentals/playground.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+left_bottom = [100, 539]
+right_bottom = [820, 539]
+apex = [500, 200]
+
+# Perform a linear fit (y=Ax+B) to each of the three sides of the triangle
+# np.polyfit returns the coefficients [A, B] of the fit
+fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
+fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
+fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
+
+print(fit_left)
+print(fit_right)
+print(fit_bottom)
---
+++ computer_vision_fundamentals/region_masking_color_combo.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# Read in the image
+image = mpimg.imread('test.jpg')
+
+# Grab the x and y size and make a copy of the image
+ysize = image.shape[0]
+xsize = image.shape[1]
+color_select = np.copy(image)
+line_image = np.copy(image)
+
+# Define color selection criteria
+# MODIFY THESE VARIABLES TO MAKE YOUR COLOR SELECTION
+red_threshold = 200
+green_threshold = 200
+blue_threshold = 200
+
+rgb_threshold = [red_threshold, green_threshold, blue_threshold]
+
+# Define the vertices of a triangular mask.
+# Keep in mind the origin (x=0, y=0) is in the upper left
+# MODIFY THESE VALUES TO ISOLATE THE REGION
+# WHERE THE LANE LINES ARE IN THE IMAGE
+left_bottom = [100, 539]
+right_bottom = [820, 539]
+apex = [500, 200]
+
+# Perform a linear fit (y=Ax+B) to each of the three sides of the triangle
+# np.polyfit returns the coefficients [A, B] of the fit
+# i.e. for fit_left or something y=0.5x+1
+fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
+fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
+fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
+
+# Mask pixels below the threshold
+# for each threshold, we've got to see if the corresponding pixel value is under the value
+# TODO: figure out how the logic of this works. Are we pulling in values if they're just under ONE
+# of the threshold values?
+color_thresholds = (image[:,:,0] < rgb_threshold[0]) | \
+                    (image[:,:,1] < rgb_threshold[1]) | \
+                    (image[:,:,2] < rgb_threshold[2])
+
+# Find the region inside the lines
+# this is using y = Ax + b but now swapping in our
+# A and b values that we calculated up above
+# TODO: look into meshgrid, what is that returning?
+XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
+region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
+                    (YY > (XX*fit_right[0] + fit_right[1])) & \
+                    (YY < (XX*fit_bottom[0] + fit_bottom[1]))
+
+# Mask color and region selection
+# TODO: what is this syntax here?
+# Set everything to black (0,0,0) that is either in the
+# color_thresholds, i.e. below our thresholds, or is
+# not in the region_thresholds
+color_select[color_thresholds | ~region_thresholds] = [0, 0, 0]
+# Color pixels red where both color and region selections met
+line_image[~color_thresholds & region_thresholds] = [255, 0, 0]
+
+# Display the image and show region and color selections
+plt.imshow(image)
+x = [left_bottom[0], right_bottom[0], apex[0], left_bottom[0]]
+y = [left_bottom[1], right_bottom[1], apex[1], left_bottom[1]]
+plt.plot(x, y, 'b--', lw=4)
+plt.imshow(color_select)
+plt.imshow(line_image)
+
+
+## TLDR
+# We set color threshold per RGB values and set everything under those values 
+# to black, everything else stays white (what if there's white sidewalk??)
+# Then, we use our triangular coordinates to focus on where the road is in the
+# camera's frame
